616f0d29d7c639ebe02f0436bd45b9a78de25524
Isaiah woko <wokoisaiah68@gmail.com>
2024-03-24 22:31:55 +0100

    aded all

--- console.py
+++ console.py
@@ -86,6 +86,26 @@ class HBNBCommand(cmd.Cmd):
             del obj[key]
             storage.save()
 
+    def do_all(self, arg):
+        list_of_string = []
+        obj = storage.all()
+        args = arg.split()
+
+        if not arg:
+            for obj_key in obj.value():
+                list_of_string.append(str(obj_key))
+        else:
+            class_name = args[0]
+            if class_name not in _classes:
+                print("** class doesn't exist **")
+                return
+            else:
+                for obj_key in obj:
+                    if obj[obj_key].__class__.__name__ == class_name:
+                        list_of_string.append(str(obj[obj_key]))
+
+        print(list_of_string)
+
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
